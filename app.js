4e7a8e0751d49ddbc0739a59e5683442b4a58dca
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-09 15:24:30 +0700

    Fix Up

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const createError = require('http-errors');
+const express = require("express");
+const mongoose = require("mongoose");
+const createError = require("http-errors");
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 mongoose
-    .connect("mongodb://localhost/tddDB", {
-        useNewUrlParser: true,
-        useCreateIndex: true
-    })
-    .then(() => console.log(`Connected to MongoDB at mongodb://localhost/tdBB...`)
-    )
-    .catch(err => {
-        console.log("Failed to connect to MongodDB...", err);
-        process.exit();
-});
+  .connect("mongodb://localhost/tddDB", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() =>
+    console.log(`Connected to MongoDB at mongodb://localhost/tddDB...`)
+  )
+  .catch(err => {
+    console.log("Failed to connect to MongoDB...", err);
+    process.exit();
+  });
 
 const usersRouter = require("./routes/user.route");
 
 app.use("/api/users", usersRouter);
 
 app.use((req, res, next) => {
-    next(createError(404));
+  next(createError(404));
 });
 
-// Error handler
-app.use(function (err, req, res, next) {
-    // Set locals, only providing error in development
-    res.locals.message = err.message;
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
 
-    // Render the error page
-    res.status(err.status || 500);
-    res.send(err);
+  // render the error page
+  res.status(err.status || 500);
+  res.send(err);
 });
 
 module.exports = app;
